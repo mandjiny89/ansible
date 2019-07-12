de6d9593339864ab09159ca51e9d96b1aaf73a45
Patchaiappan Mandjiny <mandjiny89@gmail.com>
2019-07-12 08:37:24 -0700

    checkout commit

--- wordpress/files/database_creation.sh
+++ wordpress/files/database_creation.sh
@@ -1,7 +1,7 @@
-mysql --user=root -pdatabase <<_EOF
+sudo mysql --user=root -pdatabase <<EOF
 CREATE DATABASE wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'wordpresspassword';
 FLUSH PRIVILEGES;
 exit
-_EOF_
+EOF
 
